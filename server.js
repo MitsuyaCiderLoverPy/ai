77591d7f3e8e0922358864798aed2fb35fc28ab9
MitsuyaCiderLoverPy <107018539+MitsuyaCiderLoverPy@users.noreply.github.com>
2024-08-20 16:44:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const http = require('http');
-let html = "<h1>Hello world!</h1><p>this is the test server.</p>"
+const http = require('http')
+let fs = require("fs")
+let html = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body><h1>Hello world!</h1><p>this is the test server.</p><span style="font-size: 150px;">缶がwwwwwwwカンカンwwwwwwwwwwwwwwwwwwwwww</span></body></html>'
 
 const server = http.createServer((req, res) => {
-    if(req.method==="GET"){
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.write(html);
-    res.end();
+    if (req.method === "GET") {
+        res.writeHead(200, { 'Content-Type': 'text/html charset=utf-8' })
+        res.write(html)
+        res.end()
     }
-    else if(req.method==="POST"){
-        var data=""
-        req.on("data", function(chunk){data+=chunk})
-        .on("end", function(){
-            console.log(data);
-            res.end(html)
-        })
+    else if (req.method === "POST") {
+        var data = ""
+        req.on("data", function (chunk) { data += chunk })
+            .on("end", function () {
+                console.log(data)
+                fs.writeFileSync("chat_content.txt", JSON.parse(data)["content"])
+                res.end(html)
+            })
     }
 
-});
+})
 
-const port = 8080;
-server.listen(port);
-console.log('Server listen on port ' + port);
+const port = 8080
+server.listen(port)
+console.log('Server listen on port ' + port)
