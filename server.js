39bba8558a4abefbafb5557e6ae80d3278d58072
MitsuyaCiderLoverPy <107018539+MitsuyaCiderLoverPy@users.noreply.github.com>
2024-08-20 17:32:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ let html = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body><h1>He
 const server = http.createServer((req, res) => {
     if (req.method === "GET") {
         res.writeHead(200, { 'Content-Type': 'text/html charset=utf-8' })
-        res.write(html)
+        text=fs.readFileSync("chat_content.txt")
+        res.write(text)
         res.end()
     }
     else if (req.method === "POST") {
